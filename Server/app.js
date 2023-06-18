0e38b1f80a5e21a240dd23e16d26aff3eb13b0a6
Gowtham MA <magowtham7@gmail.com>
2023-06-19 00:44:19 +0530

    first stage implementation is done at server side

--- Server/app.js
+++ Server/app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
-
 app.use(cors());
 
-app.post("/dataa", (req, res) => {
-  res.send("data recieved successfully");
+const fileStream = fs.createWriteStream("temp-file.bin");
+let buffer = Buffer.alloc(0);
+
+const bufferSize = 1024 * 1024;
+
+app.post("/data", (req, res) => {
+  req.on("data", (chunk) => {
+    buffer = Buffer.concat([buffer, chunk]);
+    if (buffer.length > bufferSize) {
+      fileStream.write(buffer);
+      buffer = Buffer.alloc(0);
+    }
+  });
+
+  req.on("end", () => {
+    res.send("data recieved");
+  });
 });
 
 app.listen(5000, () => {
